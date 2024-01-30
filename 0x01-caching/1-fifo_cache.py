7b9f339efcce6d758759bae16f8d4080b7fec97e
joekariuki3 <joekariuki3@gmail.com>
2024-01-30 14:22:22 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""FIFOCache class"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """first in first out caching"""
+
+    def __init__(self):
+        """initialize fifocache class using BaseCaching
+        that we inherit from"""
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            # if not full
+            if not (len(self.cache_data) == super().MAX_ITEMS):
+                self.cache_data[key] = item
+            else:
+                # if full remove 1st item
+                # get list of keys and get 1st one to delete
+                all_keys = list(self.cache_data.keys())
+                first_key = all_keys[0]
+                self.cache_data.pop(first_key)
+                print("DISCARD: {}".format(first_key))
+
+                # add current/new key and item to cache
+                self.cache_data[key] = item
+
+        def get(self, key):
+            """ retrieve the key if it exists"""
+            return self.cache_data.get(key)
