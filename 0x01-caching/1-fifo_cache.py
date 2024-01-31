fcc46ea27738fedbc4314f53a145a16d56237370
joekariuki3 <joekariuki3@gmail.com>
2024-01-31 16:01:50 +0300

    make sure if data in cache and same data is to be added just update the data

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,15 +20,21 @@ class FIFOCache(BaseCaching):
             if not (len(self.cache_data) == super().MAX_ITEMS):
                 self.cache_data[key] = item
             else:
-                # if full remove 1st item
-                # get list of keys and get 1st one to delete
-                all_keys = list(self.cache_data.keys())
-                first_key = all_keys[0]
-                self.cache_data.pop(first_key)
-                print("DISCARD: {}".format(first_key))
+                # if full and key is present update key value
+                # delete the key then add it again
+                if key in self.cache_data:
+                    self.cache_data.pop(key)
+                    self.cache_data[key] = item
+                else:
+                    # if full and key is new  remove 1st item
+                    # get list of keys and get 1st one to delete
+                    all_keys = list(self.cache_data.keys())
+                    first_key = all_keys[0]
+                    self.cache_data.pop(first_key)
+                    print("DISCARD: {}".format(first_key))
 
-                # add current/new key and item to cache
-                self.cache_data[key] = item
+                    # add current/new key and item to cache
+                    self.cache_data[key] = item
 
         def get(self, key):
             """ retrieve the key if it exists"""
