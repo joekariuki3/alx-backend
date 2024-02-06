50009aea2b60a1bffbd04fa4ab93a2223ce7dac0
joekariuki3 <joekariuki3@gmail.com>
2024-02-06 16:49:49 +0300

    render 3-index.html

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -30,7 +30,7 @@ def get_locale():
 @app.route('/')
 def home():
     """returns the home default page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
